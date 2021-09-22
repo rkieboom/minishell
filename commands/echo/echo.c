7e708523016c9dbac12bbce89e2313662bb5bc97
Sid Pelle <spelle@f0r5s7.codam.nl>
2021-09-22 13:07:08 +0200

    fixed memleaks + echo option

--- commands/echo/echo.c
+++ commands/echo/echo.c
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/02 14:14:40 by rkieboom      #+#    #+#                 */
-/*   Updated: 2021/05/25 16:54:41 by rkieboom      ########   odam.nl         */
+/*   Updated: 2021/09/22 13:05:30 by spelle        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,23 @@
 int	ft_echo(t_list *v, char **str)
 {
 	int	i;
+	int	newlineoption;
 
 	i = 1;
+	newlineoption = 0;
+	if (str[i] && !ft_strncmp(str[i], "-n", 3))
+	{
+		i++;
+		newlineoption = 1;
+	}
 	while (str[i])
 	{
 		ft_putstr_fd(str[i], 1);
-		if (str[i + 1])
-			ft_putchar_fd(' ', 1);
 		i++;
+		if (str[i])
+			ft_putchar_fd(' ', 1);
 	}
-	ft_putchar_fd('\n', 1);
+	if (!newlineoption)
+		ft_putchar_fd('\n', 1);
 	return (0);
 }
--- commands/unset/unset.c
+++ commands/unset/unset.c
@@ -6,7 +6,7 @@
 /*   By: spelle <spelle@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/19 20:01:05 by spelle        #+#    #+#                 */
-/*   Updated: 2021/09/16 17:13:34 by rkieboom      ########   odam.nl         */
+/*   Updated: 2021/09/21 14:55:50 by spelle        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ static void	remove_env(t_env *v, char *envname)
 	if (!ft_strncmp(current->name, envname, ft_strlen(envname)))
 	{
 		prev->next = prev->next->next;
+		free(current->name);
+		free(current->content);
 		free(current);
 	}
 }
@@ -35,18 +37,16 @@ static void	remove_env(t_env *v, char *envname)
 int	unset(t_env *v, char **str)
 {
 	int		i;
-	char	*envname;
 
 	if (str[1] == NULL)
 		return (0);
 	i = 1;
 	while (str[i] != NULL)
 	{
-		envname = ft_substr(str[i], 0, ft_strlen(str[i]));
-		if (!ft_strncmp(search_envname_returnenvname(v, envname), "", 1))
+		if (!ft_strncmp(search_envname_returnenvname(v, str[i]), "", 1))
 			ft_putstr_fd("", 1);
 		else
-			remove_env(v, envname);
+			remove_env(v, str[i]);
 		i++;
 	}
 	return (0);
