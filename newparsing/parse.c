7230efd3a1988f9794e592da092bf92edc43ed94
rkieboom <rowankieboom015@gmail.com>
2021-11-30 02:32:51 +0100

    Finished the new parsing

--- newparsing/parse.c
+++ newparsing/parse.c
@@ -6,13 +6,27 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/27 15:00:52 by rkieboom      #+#    #+#                 */
-/*   Updated: 2021/11/28 19:24:29 by rkieboom      ########   odam.nl         */
+/*   Updated: 2021/11/30 02:11:42 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 //echo $test        $?$?$"Dit is   een test'$USER  '   "'$PWD   '"$HOME    " doei
 
 #include "parse.h"
+
+static void	freemem(char **result)
+{
+	int	i;
+
+	i = 0;
+	while (result[i])
+	{
+		free(result[i]);
+		i++;
+	}
+	if (result)
+		free(result);
+}
 
 void	parsing(t_list *list)
 {
@@ -37,24 +51,77 @@ void	parsing(t_list *list)
 	}
 }
 
+int	parse_arraysize(char **str, t_list *list)
+{
+	int	i;
+	int	j;
+	int	p;
+	int	length;
+
+	i = 0;
+	j = 0;
+	p = 0;
+	length = 0;
+	while (str[j])
+	{
+		if (check_char_str(list, str[j]) == 0)
+			length++;
+		else
+		{
+			length++;
+			if (check_char(&str[j][i]))
+				p = 1;
+			else
+				p = 0;
+			while (str[j][i])
+			{	
+				check_quote(list, &str[j][i]);
+				if (list->parse.comma1 == 0 && list->parse.comma2 == 0 && ((p == 1 && !check_char(&str[j][i]) && array_thingy(&p))))
+					length++;
+				else if (list->parse.comma1 == 0 && list->parse.comma2 == 0 && (((p == 0 && check_char(&str[j][i]) && array_thingy(&p)))))
+					length++;
+				i++;
+			}
+		}
+		i = 0;
+		j++;
+	}
+	return (length);
+}
+
 void	new_parse(t_list *list)
 {
 	int i;
+	int j;
 	int length;
 	char **splitted;
 
 	i = 0;
+	j = 0;
 	length = 0;
 	splitted = parse_split_commands(list, ';');
 	while (splitted[length])
 		length++;
 	list->parse.commands = (char ***)malloc((length + 1) * sizeof(char **));
 	list->parse.commands[length] = 0;
+	list->tokens = calloc(length, sizeof(t_tokens));
 	while (length)
 	{
 		list->parse.commands[i] = parse_split_spaces(list, splitted[i], ' ');
+		parse_split_tokens(list, parse_arraysize(list->parse.commands[i], list), i);// gaat wss goed
+		while (list->parse.commands[i][j])
+			j++;
+
+		j = 0;
+		//hier tokens herkennen en in struct aangeven. Nog een nieuwe dynamische struct maken ook eerst!
 		length--;
 		i++;
 	}
+	freemem(splitted);
+	tokens(list);
+	// for(int j = 0; list->parse.commands[j]; j++)
+	// 	for(int k = 0; list->parse.commands[j][k]; k++)
+	// 		printf("%i. %s\n", k, list->parse.commands[j][k]);
+	// while(1);
 	parsing(list);
 }
