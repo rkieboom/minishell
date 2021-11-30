d787426e2664870e876f5741da9aee697781a117
rkieboom <rowankieboom015@gmail.com>
2021-11-30 02:32:51 +0100

    Finished the new parsing

--- newparsing/parse.h
+++ newparsing/parse.h
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/27 15:01:52 by rkieboom      #+#    #+#                 */
-/*   Updated: 2021/11/28 15:31:37 by rkieboom      ########   odam.nl         */
+/*   Updated: 2021/11/30 01:09:32 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,16 @@ void	new_parse(t_list *list);
 
 char	**parse_split_commands(t_list *list, char c);
 char	**parse_split_spaces(t_list *list, const char *str, char c);
+char	**parse_split_tokens(t_list *list, int size, int k);
 
-
+void	tokens(t_list *list);
 char	*checkword(t_list *list, char *str);
-char *createstring(t_list *list, char *str, int length);
+char	*createstring(t_list *list, char *str, int length);
 
 void	check_quote(t_list *list, char *c);
+int		check_char_str(t_list *list, char *str);
+int		check_char(char *c);
+
+int		array_thingy(int *p);
 
 #endif
