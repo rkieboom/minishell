78c6977e62b55795a1444a9068b80d76ca8ce89b
rkieboom <rowankieboom015@gmail.com>
2021-11-30 02:33:17 +0100

    Added free for tokens

--- free/free_parse_commands.c
+++ free/free_parse_commands.c
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/29 10:34:48 by rkieboom      #+#    #+#                 */
-/*   Updated: 2021/11/27 14:50:04 by rkieboom      ########   odam.nl         */
+/*   Updated: 2021/11/30 02:03:24 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,19 @@ void	free_parse_commands(t_list *list)
 	while (list->cmd.size > 0)
 	{
 		if (list->cmd.free[list->cmd.size - 1] == 1)
-			free(list->cmd.cmd[list->cmd.size]);
+			free(list->cmd.cmd[list->cmd.size - 1]);
 		list->cmd.size--;
+	}
+	while (list->parse.commands[i])
+	{
+		while(list->tokens[i].total)
+		{
+			free(list->tokens[i].token[list->tokens[i].total - 1]);
+			list->tokens[i].total--;
+		}
+		free(list->tokens[i].token);
+		free(list->tokens[i].token_pos);
+		i++;
 	}
 	i = 0;
 	if (list->parse.commands)
