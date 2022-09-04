492760830bd50a5325010d88d89993bffbfe097b
rkieboom <rowankieboom015@gmail.com>
2022-09-04 16:26:08 +0200

    Removed empty newline

--- commands/execve/commands.c
+++ commands/execve/commands.c
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/09/07 10:17:11 by rkieboom      #+#    #+#                 */
-/*   Updated: 2022/04/23 13:26:11 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/09/04 16:08:45 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int	check_permission(char *PATH)
 		ft_putstr_fd("minishell: ", 2);
 		ft_putstr_fd(PATH, 2);
 		ft_putendl_fd(": Permission denied", 2);
-		return (1);	
+		return (1);
 	}
 	return (0);
 }
