c3223d85b7ccd7e14878602d4b41a7f33cedcf66
rkieboom <rowankieboom015@gmail.com>
2022-09-09 11:40:58 +0200

    moved functions and normed

--- newparsing/createstring.c
+++ newparsing/createstring.c
@@ -6,38 +6,59 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/28 15:03:53 by rkieboom      #+#    #+#                 */
-/*   Updated: 2022/09/05 14:55:37 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/09/06 01:12:32 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
-struct vars
+typedef struct s_vars
 {
 	int		i;
 	int		x;
 	int		length;
 	char	*str;
 	char	*newstr;
-};
-
-static void	return_dollar_value(t_list *list, struct vars *vars)
-{
 	char	*temp;
+}				t_vars;
 
-	if (vars->str[vars->i + 1] == '?')
-	{
-		temp = ft_itoa(g_ret);
-		if (!temp)
-			ft_ret_exit(1, 1);
-		ft_memcpy(vars->newstr + vars->x, temp, ft_strlen(temp));
-		vars->length -= ft_strlen(vars->newstr);
-		vars->length++;
-		while (vars->newstr[vars->x])
-			vars->x++;
-		vars->x--;
+static void	return_ret_status(t_vars *vars)
+{
+	vars->temp = ft_itoa(g_ret);
+	if (!vars->temp)
+		ft_ret_exit(1, 1);
+	ft_memcpy(vars->newstr + vars->x, vars->temp, ft_strlen(vars->temp));
+	vars->length -= ft_strlen(vars->newstr);
+	vars->length++;
+	while (vars->newstr[vars->x])
+		vars->x++;
+	vars->x--;
+	vars->i++;
+}
+
+static void	set_env_value(t_list *list, t_vars *vars)
+{
+	vars->length -= ft_strlen(\
+	search_env(list->env, vars->str + vars->i, 0));
+	vars->length++;
+	vars->temp = ft_strdup(search_env(list->env, vars->str + vars->i, 0));
+	if (!vars->temp)
+		ft_ret_exit(1, 1);
+	ft_memcpy(vars->newstr + vars->x, vars->temp, ft_strlen(vars->temp));
+	while (vars->newstr[vars->x])
+		vars->x++;
+	vars->x--;
+	vars->i++;
+	while (vars->str[vars->i] && (ft_isdigit(vars->str[vars->i]) || \
+	ft_isalpha(vars->str[vars->i]) || (vars->str[vars->i] == '_')))
 		vars->i++;
-	}
+	vars->i--;
+}
+
+static void	return_dollar_value(t_list *list, t_vars *vars)
+{
+	if (vars->str[vars->i + 1] == '?')
+		return_ret_status(vars);
 	else if (vars->str[vars->i + 1] == '\0' || \
 	(!ft_isdigit(vars->str[vars->i + 1]) && \
 	!ft_isalpha(vars->str[vars->i + 1]) && vars->str[vars->i + 1] != '_'))
@@ -46,49 +67,42 @@ static void	return_dollar_value(t_list *list, struct vars *vars)
 		return ;
 	}
 	else
-	{
-		vars->length -= ft_strlen(search_env(list->env, vars->str + vars->i, 0)); //kijken of niet 0 ret
-		vars->length++;
-		temp = ft_strdup(search_env(list->env, vars->str + vars->i, 0));
-		if (!temp)
-			ft_ret_exit(1, 1);
-		ft_memcpy(vars->newstr + vars->x, temp, ft_strlen(temp));
-		while (vars->newstr[vars->x]) //kijken of niet 0?
-			vars->x++;
-		vars->x--;
-		vars->i++;
-		while (vars->str[vars->i] && (ft_isdigit(vars->str[vars->i]) || \
-		ft_isalpha(vars->str[vars->i]) || (vars->str[vars->i] == '_')))
-			vars->i++;
-		vars->i--;
-	}
-	free(temp);
+		set_env_value(list, vars);
+	free(vars->temp);
 }
 
-
-char *createstring(t_list *list, char *str, int length)
+static void	init_vars(t_list *list, t_vars *vars, int length, char *str)
 {
-	struct vars vars;
-
-	ft_bzero(&vars, sizeof(vars));
-	vars.length = length;
-	vars.str = str;
-	vars.newstr = ft_calloc(vars.length + 1, sizeof(char));
-	if (!vars.newstr)
+	ft_bzero(vars, sizeof(t_vars));
+	vars->length = length;
+	vars->str = str;
+	vars->newstr = ft_calloc(vars->length + 1, sizeof(char));
+	if (!vars->newstr)
 		ft_ret_exit(1, 1);
 	list->parse.comma1 = 0;
 	list->parse.comma2 = 0;
-	vars.i = -1;
+	vars->i = -1;
+}
+
+char	*createstring(t_list *list, char *str, int length)
+{
+	t_vars	vars;
+
+	init_vars(list, &vars, length, str);
 	while (vars.length > 0)
 	{
 		vars.i++;
 		check_quote(list, &str[vars.i]);
 		if (str[vars.i] == '$' && list->parse.comma1 == 0)
 			return_dollar_value(list, &vars);
-		else if (str[vars.i] == '\"' && ((list->parse.comma1 == 0 && list->parse.comma2 == 1) || (list->parse.comma1 == 0 && list->parse.comma2 == 0)))
-			continue;
-		else if (str[vars.i] == '\'' && ((list->parse.comma2 == 0 && list->parse.comma1 == 1) || (list->parse.comma1 == 0 && list->parse.comma2 == 0)))
-			continue;
+		else if (str[vars.i] == '\"' && ((list->parse.comma1 == 0 \
+		&& list->parse.comma2 == 1) || (list->parse.comma1 == 0 \
+		&& list->parse.comma2 == 0)))
+			continue ;
+		else if (str[vars.i] == '\'' && ((list->parse.comma2 == 0 \
+		&& list->parse.comma1 == 1) || (list->parse.comma1 == 0 \
+		&& list->parse.comma2 == 0)))
+			continue ;
 		else
 			vars.newstr[vars.x] = str[vars.i];
 		vars.length--;
--- newparsing/parse.h
+++ newparsing/parse.h
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/27 15:01:52 by rkieboom      #+#    #+#                 */
-/*   Updated: 2021/11/30 01:09:32 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/09/07 00:34:35 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 
 # include "../header.h"
 
-
 void	new_parse(t_list *list);
+int		parse_arraysize(char **str, t_list *list);
 
 char	**parse_split_commands(t_list *list, char c);
 char	**parse_split_spaces(t_list *list, const char *str, char c);
-char	**parse_split_tokens(t_list *list, int size, int k);
+void	parse_split_tokens(t_list *list, int size, int k);
 
 void	tokens(t_list *list);
 char	*checkword(t_list *list, char *str);
@@ -29,6 +29,8 @@ char	*createstring(t_list *list, char *str, int length);
 void	check_quote(t_list *list, char *c);
 int		check_char_str(t_list *list, char *str);
 int		check_char(char *c);
+
+void	set_heredoc(t_list *list, int k);
 
 int		array_thingy(int *p);
 
