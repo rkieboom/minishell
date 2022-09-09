2acb448f35508dae2ad99036d5e6fd573203cdda
rkieboom <rowankieboom015@gmail.com>
2022-09-09 11:38:10 +0200

    new heredoc function

--- cmd/tokens_cmd/tokens.h
+++ cmd/tokens_cmd/tokens.h
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/27 03:29:49 by rkieboom      #+#    #+#                 */
-/*   Updated: 2022/08/29 14:23:38 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/09/08 15:16:59 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,6 @@
 void	count_tokens(t_list *v, t_newcommand *temp, int k);
 void	init_new_tokens(t_newcommand *cmd);
 void	set_tokens(t_list *v, t_newcommand *pipes, int k);
+void	set_heredoc_tokens(t_list *list, t_newcommand *cmd, int k);
 
 #endif
