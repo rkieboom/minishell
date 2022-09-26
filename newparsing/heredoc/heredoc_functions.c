f8febd62eda0363de27dcaa7b44f73166c938038
rkieboom <rowankieboom015@gmail.com>
2022-09-26 15:24:49 +0200

    norminette

--- newparsing/heredoc/heredoc_functions.c
+++ newparsing/heredoc/heredoc_functions.c
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/09/06 00:23:24 by rkieboom      #+#    #+#                 */
-/*   Updated: 2022/09/25 17:45:21 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/09/26 15:21:13 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,46 @@ static void	heredoc_allocation(t_list *list, int k)
 		{
 			list->tokens[i].heredoc = \
 			ft_calloc(sizeof(t_heredoc), \
-			list->tokens[i].double_redirection_left); //leak
+			list->tokens[i].double_redirection_left);
 			if (!list->tokens[i].heredoc)
 				ft_ret_exit(1, 1);
 		}
+		i++;
+	}
+}
+
+void	get_heredoc_eof(t_list *list, int k)
+{
+	int	total;
+	int	c;
+	int	token_c;
+
+	c = 0;
+	token_c = 0;
+	total = list->tokens[k].double_redirection_left;
+	while (total)
+	{
+		if (!ft_strncmp(list->tokens[k].token[token_c], "<<", 3))
+		{
+			list->tokens[k].heredoc[c].eof = \
+			list->parse.commands[k][list->tokens[k].token_pos[token_c] + 1];
+			total--;
+			c++;
+		}
+		token_c++;
+	}
+}
+
+void	allocate_heredoc(t_list *list, int k)
+{
+	int	i;
+
+	i = 0;
+	heredoc_allocation(list, k);
+	while (i < k)
+	{
+		if (list->tokens[i].double_redirection_left)
+			get_heredoc_q(list, i);
 		i++;
 	}
 }
@@ -36,12 +72,10 @@ void	set_heredoc(t_list *list, int k)
 	int	i;
 
 	i = 0;
-	heredoc_allocation(list, k);
 	while (i < k)
 	{
 		if (list->tokens[i].double_redirection_left)
 		{
-			get_heredoc_q(list, i);
 			get_heredoc_eof(list, i);
 			get_heredoc_input(list, i);
 			heredoc_parse(list, i);
