a063ca97a5740c226758da10b843f54e5ad82199
rkieboom <rowankieboom015@gmail.com>
2022-09-30 00:35:26 +0200

    Fixed crashed with invalid redirect

--- newexecute/execute.c
+++ newexecute/execute.c
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/04/10 18:05:03 by rkieboom      #+#    #+#                 */
-/*   Updated: 2022/09/21 03:22:28 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/09/30 00:13:17 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,17 @@ static void	go(t_list *list, t_newcommand *temp, t_newcommand *v)
 		run_cmd(list, temp->command);
 	else if (!temp->next)
 	{
-		get_last_redir(temp, 0, temp->tokens->total);
-		if (temp->tokens->double_redirection_left > 0 && \
-		!ft_strncmp(temp->command[v->tokens->last_l], "<<", 3))
-			run_cmd_heredoc(list, temp);
-		else
+		if (!get_last_redir(temp, 0, temp->tokens->total))
 		{
-			if (!redirections(list, temp) && temp->command)
-				run_cmd_redir(list, temp);
-			reset_redirections(list, temp);
+			if (temp->tokens->double_redirection_left > 0 && \
+			!ft_strncmp(temp->command[v->tokens->last_l], "<<", 3))
+				run_cmd_heredoc(list, temp);
+			else
+			{
+				if (!redirections(list, temp) && temp->command)
+					run_cmd_redir(list, temp);
+				reset_redirections(list, temp);
+			}
 		}
 	}
 	else
