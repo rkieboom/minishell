4a78f8843e6f8285a429c0a7777c55b9e5af9642
rkieboom <rowankieboom015@gmail.com>
2022-10-05 00:25:59 +0200

    updated header

--- newexecute/execute.h
+++ newexecute/execute.h
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/29 10:59:45 by rkieboom      #+#    #+#                 */
-/*   Updated: 2022/09/23 17:36:26 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/10/04 23:49:31 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,16 @@ int		set_redir(t_list *list, t_newcommand *v);
 void	reset_redirections(t_list *list, t_newcommand *v);
 char	**set_cmd(t_newcommand *cmd);
 
-void	single_redirection_right(t_newcommand *v, int i);
+int		single_redirection_right(t_newcommand *v, int i);
 int		single_redirection_left(t_newcommand *v, int i);
-void	double_redirection_right(t_newcommand *v, int i);
+int		double_redirection_right(t_newcommand *v, int i);
 void	double_redirection_left(t_newcommand *v, int i);
 int		get_last_redir(t_newcommand *v, int i, int total);
 
 void	run_cmd(t_list *list, char **cmd);
 void	run_cmd_redir(t_list *list, t_newcommand *cmd);
-void	run_cmd_heredoc(t_list *list, t_newcommand *cmd);
+
+void	write_to_pipe(t_heredoc *heredoc, t_heredoc_data *data);
+void	clear_pipe_heredoc(t_heredoc *heredoc);
 
 #endif
