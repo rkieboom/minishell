9268745a6c9fbbb7f669adfb2b3b06084a131a6f
rkieboom <rowankieboom015@gmail.com>
2022-10-05 12:49:02 +0200

    bump

--- newexecute/pipes/pipes.c
+++ newexecute/pipes/pipes.c
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/05/19 16:17:04 by rkieboom      #+#    #+#                 */
-/*   Updated: 2022/10/04 23:09:38 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/10/05 01:04:11 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ static void	init_vars(t_vars *var, t_newcommand *v)
 {
 	var->temp = v;
 	var->i = 0;
+	var->error = 0;
 	while (v)
 	{
 		v = v->next;
@@ -43,16 +44,17 @@ static void	go(t_list *list, t_vars *vars)
 		if (vars->temp->tokens->heredoc)
 			write_to_pipe(vars->temp->tokens->heredoc, \
 			vars->temp->tokens->heredoc->data);
-		if (!redirections(list, vars->temp) && vars->temp->command)
+		if (!redirections(list, vars->temp) && vars->temp->command && vars->error == 0)
 			run_cmd_redir(list, vars->temp);
 	}
-	else
+	else if (vars->error == 0)
 		run_cmd_redir(list, vars->temp);
 	if (vars->temp->tokens && vars->temp->tokens->total > 0)
 		reset_redirections(list, vars->temp);
 	if (vars->temp->tokens->heredoc)
 		clear_pipe_heredoc(vars->temp->tokens->heredoc);
 	clear_pipes(vars);
+	vars->error = 0;
 }
 
 void	ft_pipes(t_list *list, t_newcommand *v)
@@ -63,10 +65,7 @@ void	ft_pipes(t_list *list, t_newcommand *v)
 	while (vars.temp)
 	{
 		if (get_last_redir(vars.temp, 0, vars.temp->tokens->total))
-		{
-			vars.temp = vars.temp->next;
-			continue ;
-		}
+			vars.error = 1;
 		else
 			go(list, &vars);
 		vars.i++;
