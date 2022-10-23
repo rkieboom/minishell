ca159c9b918a013d9624acddc21ed5dd87dba2e2
rkieboom <rowankie37@outlook.com>
2022-10-23 02:43:36 +0200

    Prototypes changed

--- execute/run_cmd.c
+++ execute/run_cmd.c
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/10/11 15:34:15 by rkieboom      #+#    #+#                 */
-/*   Updated: 2022/10/12 01:20:05 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/10/23 02:07:09 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ void	run_cmd(t_list *list, char **cmd)
 	else if (!ft_strncmp(cmd[0], "pwd", 4))
 		g_global.status = pwd();
 	else if (!ft_strncmp(cmd[0], "export", 7))
-		g_global.status = export(list->env, cmd);
+		g_global.status = export(&list->env, cmd);
 	else if (!ft_strncmp(cmd[0], "unset", 6))
-		g_global.status = unset(list->env, cmd);
+		g_global.status = unset(&list->env, cmd);
 	else if (!ft_strncmp(cmd[0], "env", 4))
 		g_global.status = env(list->env);
 	else if (!ft_strncmp(cmd[0], "exit", 5))
