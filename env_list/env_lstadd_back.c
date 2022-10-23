3330ce63fc301788103688bd1b3ad8875d52fc66
rkieboom <rowankie37@outlook.com>
2022-10-23 02:40:49 +0200

    Fixed when list is empty

--- env_list/env_lstadd_back.c
+++ env_list/env_lstadd_back.c
@@ -6,17 +6,24 @@
 /*   By: spelle <spelle@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/09 14:53:16 by spelle        #+#    #+#                 */
-/*   Updated: 2021/03/09 14:57:54 by spelle        ########   odam.nl         */
+/*   Updated: 2022/10/23 01:57:57 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "env_list.h"
 
+//Adds new t_env at the back of the list
 void	env_lstadd_back(t_env **lst, t_env *new)
 {
 	t_env	*temp;
 
 	temp = *lst;
-	temp = env_lstlast(temp);
-	temp->next = new;
+	if (!temp)
+		*lst = new;
+	else
+	{
+		temp = env_lstlast(temp);
+		temp->next = new;
+		new->prev = temp;
+	}
 }
