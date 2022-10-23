acda063c4cd0875162e0f022ff77f2456f1477c0
rkieboom <rowankie37@outlook.com>
2022-10-23 02:37:47 +0200

    Updated header

--- builtin/commands.h
+++ builtin/commands.h
@@ -6,7 +6,7 @@
 /*   By: rkieboom <rkieboom@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/05/11 16:35:27 by rkieboom      #+#    #+#                 */
-/*   Updated: 2022/10/18 03:23:53 by rkieboom      ########   odam.nl         */
+/*   Updated: 2022/10/23 02:05:55 by rkieboom      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ int		cd(t_list *v, char **str);
 int		env(t_env *v);
 int		pwd(void);
 int		echo(char **str);
-int		export(t_env *v, char **str);
-int		unset(t_env *v, char **str);
+int		export(t_env **v, char **str);
+int		unset(t_env **v, char **str);
 void	ft_execve(t_list *list, char **str);
 void	ft_ret_exit(int ret, int print);
 void	ft_exit(char **str);
@@ -40,14 +40,14 @@ int		cd_check_permissions(char *path);
 int		exec_cmd(t_env *env, char *path);
 int		cd_tilde_expansion(t_list *list, char *str);
 
-char	*add_new_line(char *str);
 char	*relative_path(char *PATH, t_env *env);
 char	*absolute_path(char *PATH);
 int		is_absolute_path(char *str);
 int		check_exist(char *PATH);
 int		check_permission(char *PATH);
 void	export_declare_list(t_env *v);
-void	ft_swap(char **s1, char **s2);
+void	ft_swap(char **s1, char **s2);//Not ness
 void	signals(void);
+void	signals_DFL(void);
 
 #endif
